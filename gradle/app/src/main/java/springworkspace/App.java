c37af5287470d23c3b390a1c985b7eae24ff987a
parksungeon <un109101@naver.com>
2024-04-11 16:47:56 +0900

    2024-04-11 16:47

--- gradle/app/src/main/java/springworkspace/App.java
+++ gradle/app/src/main/java/springworkspace/App.java
@@ -3,7 +3,6 @@ package springworkspace;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
